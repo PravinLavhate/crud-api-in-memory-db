b565e3c4d1033ff2b153871d58a295fa6a2ca15c
pravin lavhate <pravinlavhate@pravins-MacBook-Air.local>
2024-02-24 17:50:52 +0530

    feat: Add user retrieval, deletion, and update APIs
    This commit adds the following API endpoints for user management:
    
    - GET /api/users/{userId}: Retrieves a user by their user ID.
    - DELETE /api/users/{userId}: Deletes a user by their user ID.
    - PUT /api/users/{userId}: Updates a user's information.
    
    The implementation includes logic for retrieving, deleting, and updating user data based on the provided user ID. Error handling is also implemented to gracefully handle cases where the user is not found or invalid input is provided.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # crud-api-in-memory-db
+
+-Endpoints:
+
+- Retrieve all users (`GET /api/users`)
+- Add a new user (`POST /api/users`)
+- Rretrieve a user by their user ID (`GET /api/users/{userId}`)
+- Update a user's information (`PUT /api/users/{userId}`)
+- Delete a user by their user ID (`DELETE /api/users/{userId}`)
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.use(body_parser.json());
 //in memory database
 let users = [];
 
+//get user list
 router.get("/", (req, resp) => {
   resp.json(users);
 });
@@ -30,6 +31,51 @@ router.post("/", (req, resp) => {
   users.push(nUser);
 
   resp.status(201).json(nUser);
+});
+
+//get user with userid
+router.put("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const { username, age, hobbies } = req.body;
+  const userIndex = users.findIndex((user) => user.id === userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  users[userIndex] = {
+    ...users[userIndex],
+    username: username || users[userIndex].username,
+    age: age || users[userIndex].age,
+    hobbies: hobbies || users[userIndex].hobbies,
+  };
+  res.json(users[userIndex]);
+});
+
+//update user
+router.put("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const { username, age, hobbies } = req.body;
+  const userIndex = users.findIndex((user) => user.id === userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  users[userIndex] = {
+    ...users[userIndex],
+    username: username || users[userIndex].username,
+    age: age || users[userIndex].age,
+    hobbies: hobbies || users[userIndex].hobbies,
+  };
+  res.json(users[userIndex]);
+});
+
+// Delete an existing user
+router.delete("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const userIndex = users.findIndex((user) => user.id === userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  users.splice(userIndex, 1);
+  res.sendStatus(204).json({ msg: "record is deleted" });
 });
 
 module.exports = router;
